2f319f892f82edb3d495dc08358ad2d4033b6228
tanat1994 <tanat1994@gmail.com>
2018-06-18 16:19:58 +0700

    added serve static

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.get('/api/generate', (req, res) => {
   res.send(dummy);
 });
